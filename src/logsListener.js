e710c236792d8622fe9c3a59a03d5da63d6195dd
SuperCraft3M <supercraft3m@gmail.com>
2024-05-04 19:19:48 +0200

    Ca va l'égo hein

--- src/logsListener.js
+++ src/logsListener.js
@@ -27,7 +27,7 @@ module.exports.start = async () => {
             server.logsListener.close();
 
             if (server.state === "started") {
-                server.log("[raraph.fr] Process exited with code " + event.Actor.Attributes.exitCode + ". Restarting in 3 seconds...", Math.floor(event.timeNano / 1000000));
+                server.log("[AutoDeploy] Process exited with code " + event.Actor.Attributes.exitCode + ". Restarting in 3 seconds...", Math.floor(event.timeNano / 1000000));
                 server.state = "restarting";
                 setTimeout(() => {
                     if (server.state === "restarting") server.container.start().catch(() => { });
