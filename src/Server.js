80fc52c49a29ebeebd86ea0bf60c95fe3597f10d
Raraph84 <contact@raraph.fr>
2025-09-13 10:38:00 +0200

    Refactor server initialization by merging nodejs/python and website/reactjs

--- src/Server.js
+++ src/Server.js
@@ -52,113 +52,74 @@ module.exports = class Server {
                 delete serverInfos.port;
                 delete serverInfos.destPort;
             }
-
-            if (serverInfos.type === "nodejs") {
-
-                const container = containers.find((container) => container.Names[0] === "/" + serverInfos.name);
-
-                if (container) {
-
-                    const server = new NodeJsServer(serverInfos.name, docker.getContainer(container.Id), gateway, serverInfos.dockerImage, serverInfos.deployment ?? null);
-
-                    if (container.State === "running") {
-                        server.listenLogs();
-                        server.setState("started");
-                    } else {
-                        server.deploy();
-                    }
-
-                } else {
-
-                    if (!existsSync(path.join(os.homedir(), "servers", serverInfos.name)))
-                        await fs.mkdir(path.join(os.homedir(), "servers", serverInfos.name));
-
-                    const exposedPorts = {};
-                    const portBindings = {};
-                    for (const [hostPort, port] of Object.entries(serverInfos.ports ?? {})) {
-                        exposedPorts[port + "/tcp"] = {};
-                        portBindings[port + "/tcp"] = [{ HostIp: "127.0.0.1", HostPort: hostPort.toString() }];
-                    }
-
-                    const container = await docker.createContainer({
-                        name: serverInfos.name,
-                        ...(serverInfos.ports ? { ExposedPorts: exposedPorts } : {}),
-                        Tty: true,
-                        OpenStdin: true,
-                        HostConfig: {
-                            Binds: [path.join(os.homedir(), "servers", serverInfos.name) + ":/home/server"],
-                            ...(serverInfos.ports ? { PortBindings: portBindings } : { NetworkMode: "host" }),
-                            LogConfig: { Type: "json-file", Config: { "max-size": "5m", "max-file": "2" } }
-                        },
-                        WorkingDir: "/home/server",
-                        Env: Object.entries(serverInfos.environmentVariables ?? {}).map((environmentVariable) => environmentVariable[0] + "=" + environmentVariable[1]),
-                        Image: serverInfos.dockerImage,
-                        Cmd: serverInfos.startCommand ?? ("node " + (serverInfos.mainFile ?? "index.js"))
-                    });
-
-                    const server = new NodeJsServer(serverInfos.name, container, gateway, serverInfos.dockerImage, serverInfos.deployment ?? null);
-                    server.deploy();
-                }
-
-            } else if (serverInfos.type === "python") {
-
-                const container = containers.find((container) => container.Names[0] === "/" + serverInfos.name);
-
-                if (container) {
-
-                    const server = new PythonServer(serverInfos.name, docker.getContainer(container.Id), gateway, serverInfos.dockerImage, serverInfos.deployment ?? null);
-
-                    if (container.State === "running") {
-                        server.listenLogs();
-                        server.setState("started");
-                    } else {
-                        server.deploy();
-                    }
-
-                } else {
-
-                    if (!existsSync(path.join(os.homedir(), "servers", serverInfos.name)))
-                        await fs.mkdir(path.join(os.homedir(), "servers", serverInfos.name));
-
-                    const exposedPorts = {};
-                    const portBindings = {};
-                    for (const [hostPort, port] of Object.entries(serverInfos.ports ?? {})) {
-                        exposedPorts[port + "/tcp"] = {};
-                        portBindings[port + "/tcp"] = [{ HostIp: "127.0.0.1", HostPort: hostPort.toString() }];
-                    }
-
-                    const container = await docker.createContainer({
-                        name: serverInfos.name,
-                        ...(serverInfos.ports ? { ExposedPorts: exposedPorts } : {}),
-                        Tty: true,
-                        OpenStdin: true,
-                        HostConfig: {
-                            Binds: [path.join(os.homedir(), "servers", serverInfos.name) + ":/home/server"],
-                            ...(serverInfos.ports ? { PortBindings: portBindings } : { NetworkMode: "host" }),
-                            LogConfig: { Type: "json-file", Config: { "max-size": "5m", "max-file": "2" } }
-                        },
-                        WorkingDir: "/home/server",
-                        Env: Object.entries(serverInfos.environmentVariables ?? {}).map((environmentVariable) => environmentVariable[0] + "=" + environmentVariable[1]),
-                        Image: serverInfos.dockerImage,
-                        Cmd: "if [ -f requirements.txt ]; then pip install --no-cache-dir -r requirements.txt; fi && " + (serverInfos.startCommand ?? ("python " + (serverInfos.mainFile ?? "main.py")))
-                    });
-
-                    const server = new PythonServer(serverInfos.name, container, gateway, serverInfos.dockerImage, serverInfos.deployment ?? null);
-                    server.deploy();
-                }
-
-            } else if (serverInfos.type === "website") {
-
-                const server = new WebsiteServer(serverInfos.name, serverInfos.deployment ?? null);
-                if (!existsSync(path.join(os.homedir(), "servers", serverInfos.name)))
-                    server.deploy();
-
-            } else if (serverInfos.type === "reactjs") {
-
-                if (serverInfos.buildDockerImage && serverInfos.deployment && !serverInfos.deployment.dockerImage)
+            if (serverInfos.type === "reactjs") {
+                if (serverInfos.buildDockerImage && serverInfos.deployment && !serverInfos.deployment.dockerImage) {
+                    console.warn("The 'buildDockerImage' field has been deprecated, it will be removed in future versions. Please use 'deployment.dockerImage' instead.");
                     serverInfos.deployment.dockerImage = serverInfos.buildDockerImage;
+                }
+            }
 
-                const server = new ReactJsServer(serverInfos.name, serverInfos.deployment ?? null);
+            if (serverInfos.type === "nodejs" || serverInfos.type === "python") {
+
+                const container = containers.find((container) => container.Names[0] === "/" + serverInfos.name);
+
+                if (container) {
+
+                    const server = serverInfos.type === "nodejs"
+                        ? new NodeJsServer(serverInfos.name, docker.getContainer(container.Id), gateway, serverInfos.dockerImage, serverInfos.deployment ?? null)
+                        : new PythonServer(serverInfos.name, docker.getContainer(container.Id), gateway, serverInfos.dockerImage, serverInfos.deployment ?? null);
+
+                    if (container.State === "running") {
+                        server.listenLogs();
+                        server.setState("started");
+                    } else {
+                        server.deploy();
+                    }
+
+                } else {
+
+                    if (!existsSync(path.join(os.homedir(), "servers", serverInfos.name)))
+                        await fs.mkdir(path.join(os.homedir(), "servers", serverInfos.name));
+
+                    const exposedPorts = {};
+                    const portBindings = {};
+                    for (const [hostPort, port] of Object.entries(serverInfos.ports ?? {})) {
+                        exposedPorts[port + "/tcp"] = {};
+                        portBindings[port + "/tcp"] = [{ HostIp: "127.0.0.1", HostPort: hostPort.toString() }];
+                    }
+
+                    const startCommand = serverInfos.startCommand ?? (serverInfos.type === "nodejs"
+                        ? ("node " + (serverInfos.mainFile ?? "index.js"))
+                        : ("python " + (serverInfos.mainFile ?? "main.py")));
+
+                    const container = await docker.createContainer({
+                        name: serverInfos.name,
+                        ...(serverInfos.ports ? { ExposedPorts: exposedPorts } : {}),
+                        Tty: true,
+                        OpenStdin: true,
+                        HostConfig: {
+                            Binds: [path.join(os.homedir(), "servers", serverInfos.name) + ":/home/server"],
+                            ...(serverInfos.ports ? { PortBindings: portBindings } : { NetworkMode: "host" }),
+                            LogConfig: { Type: "json-file", Config: { "max-size": "5m", "max-file": "2" } }
+                        },
+                        WorkingDir: "/home/server",
+                        Env: Object.entries(serverInfos.environmentVariables ?? {}).map((environmentVariable) => environmentVariable[0] + "=" + environmentVariable[1]),
+                        Image: serverInfos.dockerImage,
+                        Cmd: (serverInfos.type === "python" ? "if [ -f requirements.txt ]; then pip install --no-cache-dir -r requirements.txt; fi && " : "") + startCommand
+                    });
+
+                    const server = serverInfos.type === "nodejs"
+                        ? new NodeJsServer(serverInfos.name, container, gateway, serverInfos.dockerImage, serverInfos.deployment ?? null)
+                        : new PythonServer(serverInfos.name, container, gateway, serverInfos.dockerImage, serverInfos.deployment ?? null);
+                    server.deploy();
+                }
+
+            } else if (serverInfos.type === "website" || serverInfos.type === "reactjs") {
+
+                const server = serverInfos.type === "website"
+                    ? new WebsiteServer(serverInfos.name, serverInfos.deployment ?? null)
+                    : new ReactJsServer(serverInfos.name, serverInfos.deployment ?? null);
+
                 if (!existsSync(path.join(os.homedir(), "servers", serverInfos.name)))
                     server.deploy();
 
